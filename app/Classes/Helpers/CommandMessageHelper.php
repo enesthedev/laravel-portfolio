f4f57ef4d77d5a91d8605f120c05275621cea9c8
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2022-03-10 13:05:44 +0100

    Add: double-dim message array support to CommandMessageHelper

--- app/Classes/Helpers/CommandMessageHelper.php
+++ app/Classes/Helpers/CommandMessageHelper.php
@@ -6,6 +6,14 @@ use Illuminate\Console\Command;
 
 class CommandMessageHelper
 {
+    /**
+     * Creates beautiful command messages
+     *
+     * @param Command $command
+     * @param array $head
+     * @param array $messages
+     * @return Command
+     */
     public static function message(Command $command, array $head, array $messages)
     {
         $headMessage = $head[0] ?? 'Default message';
@@ -14,6 +22,16 @@ class CommandMessageHelper
         $command->line("\n <fg=default;bg=${headMessageColor}>${headMessage}</>\n");
 
         foreach ($messages as $message) {
+            if (is_array($message)) {
+                $dumpString = "";
+
+                foreach ($message as $parentMessage) {
+                    $dumpString = $dumpString . " " . $parentMessage;
+                }
+
+                $message = $dumpString;
+            }
+
             $command->line("   <fg=default>${message}</>");
 
             if (! next($messages)) {
