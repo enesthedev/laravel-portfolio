914d27fc5a0bacec4e1fac3e063faa577a20828c
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2022-03-15 14:42:02 +0100

    Add: avatar prop to component
    Refactor: Redesign ui of header

--- app/View/Components/Header.php
+++ app/View/Components/Header.php
@@ -29,16 +29,24 @@ class Header extends TailwindComponent
     public string $title;
 
     /**
+     * Avatar path of header;
+     *
+     * @var string
+     */
+    public string $avatar;
+
+    /**
      * Create a new component instance.
      *
      * @return void
      */
-    public function __construct(string $title = '', string $description = '', array $routes = [], string $styles = '')
+    public function __construct(string $title = '', string $description = '', array $routes = [], string $avatar = '', string $styles = '')
     {
         parent::__construct($styles);
 
         $this->title = $title;
         $this->description = $description;
+        $this->avatar = $avatar;
         $this->routes = $routes;
     }
 
--- resources/views/web/components/header.blade.php
+++ resources/views/web/components/header.blade.php
@@ -1,18 +1,27 @@
-<div class="flex flex-col gap-y-6">
-    @if(! empty($title))
-        <div>
-            <h1 class="font-black font-inter text-4xl">{{ $title }}</h1>
-            @if(! empty($description))
-                <p>{{ __($description) }}</p>
+<x-container styles="p-5 md:p-10 md:pb-4">
+    @if(! (empty($title) && empty($description)))
+        <div class="flex flex-col gap-y-5 md:gap-y-0 md:flex-row">
+            @if(! empty($avatar))
+                <div class="flex items-center justify-center md:w-1/3">
+                    <img src="{{ asset($avatar) }}" class="rounded-full h-[200px]"/>
+                </div>
             @endif
+            <div class="flex flex-col justify-center items-center md:w-2/3 md:items-start">
+                <h1 class="font-inter font-bold text-2xl">{{ __($title) }}</h1>
+                <p>{{ __($description) }}</p>
+            </div>
         </div>
     @endif
 
     @if($routes)
-        <nav class="flex gap-x-3 flex-col sm:ml-auto sm:flex-row">
-            @foreach($routes as $route)
-                <x-link :to="$route[1]" styles="text-sm">{{ __($route[0]) }}</x-link>
-            @endforeach
+        <nav class="mt-5 md:mt-3">
+            <ul class="text-center w-full md:text-right">
+                @foreach($routes as $route)
+                    <li class="inline-block">
+                        <x-link :to="$route['path']" styles="ml-2">{{ __($route["title"]) }}</x-link>
+                    </li>
+                @endforeach
+            </ul>
         </nav>
     @endif
-</div>
+</x-container>
