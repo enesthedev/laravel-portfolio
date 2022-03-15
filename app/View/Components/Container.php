9d074ca5f68d0b05f6181619cdcd0c33ed70f309
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2022-03-15 14:21:03 +0100

    Add: Container component for 900px fixed with divs

---
+++ app/View/Components/Container.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\View\Components;
+
+class Container extends \App\Classes\Framework\View\Component\TailwindComponent
+{
+
+    /**
+     * Create a new component instance.
+     *
+     * @param string $styles
+     */
+    public function __construct(string $styles = '')
+    {
+        parent::__construct($styles);
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Closure|\Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Support\Htmlable|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|string
+     */
+    public function render()
+    {
+        return view('web.components.container');
+    }
+}
---
+++ resources/views/web/components/container.blade.php
@@ -0,0 +1,1 @@
+<div {{ $attributes->merge(['class' => ($styles . ' w-full max-w-[900px]') ]) }}>{{ $slot }}</div>
