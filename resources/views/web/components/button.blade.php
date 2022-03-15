5c6d5fe1d7652eb7c7d39d3164db15233fd8d63d
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2022-03-15 14:12:12 +0100

    Refactor: Removed Button component from project

--- resources/views/web/components/button.blade.php
+++
@@ -1,5 +0,0 @@
-<button {{
-    $attributes->merge(
-        ['type' => $type, 'class' => $styles . ' cursor-pointer font-semibold hover:underline hover:text-blue-900']
-    )
-}}>{{ $slot }}</button>
