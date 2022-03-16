2490fd3a0536396be4af45909b12d04eb8b4cd58
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2022-03-16 12:52:35 +0100

    Add: Database connection check to if APP_DEBUG is false

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (! env('APP_DEBUG'))
+            try {
+                DB::connection()
+                    ->getPdo();
+            } catch (\Exception $e) {
+                abort($e instanceof \PDOException ? 503 : 500);
+            };
     }
 }
