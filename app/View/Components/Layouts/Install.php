c2c4bdbb8badebd9018ef8010f6cd4dabdbcf5a7
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2022-03-17 13:25:45 +0100

    Add: Install layout to application

---
+++ app/View/Components/Layouts/Install.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\View\Components\Layouts;
+
+use Illuminate\Support\HtmlString;
+use Illuminate\View\Component;
+use function view;
+
+class Install extends Component
+{
+
+    /**
+     * Additional Page scripts.
+     *
+     * @var HtmlString
+     */
+    public HtmlString $scripts;
+
+    /**
+     * Additional Page styles.
+     *
+     * @var HtmlString
+     */
+    public HtmlString $styles;
+
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct(HtmlString $styles = new HtmlString(), HtmlString $scripts = new HtmlString())
+    {
+        $this->styles = $styles;
+        $this->scripts = $scripts;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+        return view('admin.layouts.install');
+    }
+}
---
+++ resources/views/admin/layouts/install.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+    <link href="{{ asset("styles/tailwind.css") }}" rel="stylesheet">
+    <link href="{{ asset("styles/swal2.css") }}" rel="stylesheet">
+
+    {{ $styles }}
+
+    <title>Document</title>
+</head>
+<body class="antialiased flex flex-col items-center justify-center bg-white text-stone-800 dark:bg-stone-900 dark:text-stone-100 h-screen gap-y-5 sm:h-auto sm:gap-0">
+    {{ $slot }}
+</body>
+<script src="{{ asset("scripts/script.js") }}"></script>
+{{ $scripts }}
+</html>
