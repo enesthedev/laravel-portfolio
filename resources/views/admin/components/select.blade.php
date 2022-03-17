80ffee2a820e43005096d2262d46f3dbad795b76
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2022-03-17 17:39:25 +0100

    Add: Localization support to admin.select component

--- resources/views/admin/components/select.blade.php
+++ resources/views/admin/components/select.blade.php
@@ -1,8 +1,8 @@
  <div>
-     <label for="{{ $selectDomId }}" {{ $attributes->merge(['class' => ($labelStyles . ' ' . $variants[$variant] . ' block mb-2 text-sm font-medium') ]) }}>{{ $selectLabelText }}</label>
+     <label for="{{ $selectDomId }}" {{ $attributes->merge(['class' => ($labelStyles . ' ' . $variants[$variant] . ' block mb-2 text-sm font-medium') ]) }}>{{ __($selectLabelText) }}</label>
      <select id="{{ $selectDomId }}" name="{{ $selectFormName }}" {{ $attributes->merge(['class' => ($selectStyles . ' ' . $variants[$variant] . ' bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 focus:ring-blue-500 focus:border-blue-500') ]) }}>
          @foreach($options as $optionKey => $optionValue)
-             <option {{ $selectedOption === $optionValue ? 'selected' : '' }}>{{ is_array($optionValue) ? $optionKey : $optionValue }}</option>
+             <option {{ $selectedOption === $optionValue ? 'selected' : '' }}>{{ is_array($optionValue) ? __($optionKey) : __($optionValue) }}</option>
          @endforeach
      </select>
 </div>
